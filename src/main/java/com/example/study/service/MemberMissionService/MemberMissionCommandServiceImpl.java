d41b1636f0cff94d738391ad3f0de6ef9e5c4cfb
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-11-30 13:45:34 +0900

    feat: controller

--- src/main/java/com/example/study/service/MemberMissionService/MemberMissionCommandServiceImpl.java
+++ src/main/java/com/example/study/service/MemberMissionService/MemberMissionCommandServiceImpl.java
@@ -2,20 +2,14 @@ package com.example.study.service.MemberMissionService;
 
 import com.example.study.apiPayload.code.status.ErrorStatus;
 import com.example.study.apiPayload.exception.handler.MemberMissionHandler;
-import com.example.study.apiPayload.exception.handler.MissionHandler;
 import com.example.study.converter.MemberMissionConverter;
-import com.example.study.converter.MissionConverter;
 import com.example.study.domain.Member;
 import com.example.study.domain.MemberMission;
 import com.example.study.domain.Mission;
-import com.example.study.domain.Restaurant;
 import com.example.study.repository.MemberMissionRepository;
 import com.example.study.repository.MemberRepository;
 import com.example.study.repository.MissionRepository;
-import com.example.study.repository.RestaurantRepository;
-import com.example.study.validation.annotation.NotExistMemberMession;
 import com.example.study.web.dto.MemberMissionRequestDTO;
-import com.example.study.web.dto.MissionRequestDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/example/study/service/MemberMissionService/MemberMissionQueryServiceImpl.java
+++ src/main/java/com/example/study/service/MemberMissionService/MemberMissionQueryServiceImpl.java
@@ -1,11 +1,5 @@
 package com.example.study.service.MemberMissionService;
 
-import com.example.study.repository.MemberMissionRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor
-public class MemberMissionQueryServiceImpl implements MemberMissionCommandService{
-    private final MemberMissionRepository missionRepository;
+public class MemberMissionQueryServiceImpl implements MemberMissionQueryService{
 }
