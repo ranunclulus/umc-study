56e5b0d614b7002659d6f2671b33fb987ad3da45
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-11-30 11:40:36 +0900

    feat: review service

---
+++ src/main/java/com/example/study/service/ReviewService/ReviewQueryService.java
@@ -0,0 +1,4 @@
+package com.example.study.service.ReviewService;
+
+public interface ReviewQueryService {
+}
---
+++ src/main/java/com/example/study/service/ReviewService/ReviewQueryServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.study.service.ReviewService;
+
+public class ReviewQueryServiceImpl implements ReviewQueryService {
+}
